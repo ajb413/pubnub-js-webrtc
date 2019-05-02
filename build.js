9ecdc808cc6fc1bdfeab087754aa1b087a396507
Adam Bavosa <adamb@pubnub.com>
2019-05-02 10:21:27 -0700

    minified build added

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 var browserify = require("browserify");
 var babelify = require("babelify");
+var UglifyJS = require("uglify-js");
+
+var outputFilePath = "./dist/pubnub-js-webrtc.js";
+var outputMinifiedFilePath = "./dist/pubnub-js-webrtc.min.js";
 
 browserify({
         debug: false
@@ -8,4 +12,11 @@ browserify({
     .require("./src/pubnub-js-webrtc.js", { entry: true })
     .bundle()
     .on("error", function (err) { console.error("Error: " + err.message); })
-    .pipe(fs.createWriteStream("./dist/pubnub-js-webrtc.js"));
+    .pipe(fs.createWriteStream(outputFilePath))
+    .on("finish", minify);
+
+function minify() {
+    fs.writeFileSync(outputMinifiedFilePath, UglifyJS.minify({
+        [outputFilePath]: fs.readFileSync(outputFilePath, "utf8")
+    }).code, "utf8");
+}
